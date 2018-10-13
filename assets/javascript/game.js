99b4f8eb57ccb7aac5403574d7520485b20152c1
cmpsaints <cmpsaints@yandex.com>
2018-10-13 12:23:59 -0700

    a slight mod to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,21 +20,62 @@ $(document).ready(function () {
     $('#wins').text(wins);
     $('#losses').text(losses);
 
-    // Reset game
-    function reset() {
-        randomNum = Math.floor(Math.random() * 60 + 20);
-        console.log(randomNum)
-        $('#scoreToMatch').text(randomNum);
-        num1 = Math.floor(Math.random() * 10 + 1);
-        num2 = Math.floor(Math.random() * 10 + 1);
-        num3 = Math.floor(Math.random() * 10 + 1);
-        num4 = Math.floor(Math.random() * 10 + 1);
-        playerTotal = 0;
+
+    // Clicking crystals
+    $('#crystal_01').on('click', function () {
+        playerTotal = playerTotal + num1;
+        console.log("New playerTotal= " + playerTotal);
         $('#totalScore').text(playerTotal);
-    }
+        //Win & lose conditions
+        if (playerTotal == randomNum) {
+            winner();
+        }
+        else if (playerTotal > randomNum) {
+            loser();
+        }
+    })
+
+    $('#crystal_02').on('click', function () {
+        playerTotal = playerTotal + num2;
+        console.log("New playerTotal= " + playerTotal);
+        $('#totalScore').text(playerTotal);
+        if (playerTotal == randomNum) {
+            winner();
+        }
+        else if (playerTotal > randomNum) {
+            loser();
+        }
+    })
+
+    $('#crystal_03').on('click', function () {
+        playerTotal = playerTotal + num3;
+        console.log("New playerTotal= " + playerTotal);
+        $('#totalScore').text(playerTotal);
+
+        if (playerTotal == randomNum) {
+            winner();
+        }
+        else if (playerTotal > randomNum) {
+            loser();
+        }
+    })
+
+    $('#crystal_04').on('click', function () {
+        playerTotal = playerTotal + num4;
+        console.log("New playerTotal= " + playerTotal);
+        $('#totalScore').text(playerTotal);
+
+        if (playerTotal == randomNum) {
+            winner();
+        }
+        else if (playerTotal > randomNum) {
+            loser();
+        }
+    });
+
 
     // Display wins
-    function woohoo() {
+    function winner() {
         alert("Congratulations, you win!");
         wins++;
         $('#wins').text(wins);
@@ -49,55 +90,18 @@ $(document).ready(function () {
         reset()
     }
 
-    // Clicking crystals
-    $('#crystal_01').on('click', function () {
-        playerTotal = playerTotal + num1;
-        console.log("New playerTotal= " + playerTotal);
+
+    // Reset game
+    function reset() {
+        randomNum = Math.floor(Math.random() * 60 + 20);
+        console.log(randomNum)
+        $('#scoreToMatch').text(randomNum);
+        num1 = Math.floor(Math.random() * 10 + 1);
+        num2 = Math.floor(Math.random() * 10 + 1);
+        num3 = Math.floor(Math.random() * 10 + 1);
+        num4 = Math.floor(Math.random() * 10 + 1);
+        playerTotal = 0;
         $('#totalScore').text(playerTotal);
-        //Win & lose conditions
-        if (playerTotal == randomNum) {
-            woohoo();
-        }
-        else if (playerTotal > randomNum) {
-            loser();
-        }
-    })
+    }
 
-    $('#crystal_02').on('click', function () {
-        playerTotal = playerTotal + num2;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if (playerTotal == randomNum) {
-            woohoo();
-        }
-        else if (playerTotal > randomNum) {
-            loser();
-        }
-    })
-
-    $('#crystal_03').on('click', function () {
-        playerTotal = playerTotal + num3;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-
-        if (playerTotal == randomNum) {
-            woohoo();
-        }
-        else if (playerTotal > randomNum) {
-            loser();
-        }
-    })
-
-    $('#crystal_04').on('click', function () {
-        playerTotal = playerTotal + num4;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-
-        if (playerTotal == randomNum) {
-            woohoo();
-        }
-        else if (playerTotal > randomNum) {
-            loser();
-        }
-    });
 });
